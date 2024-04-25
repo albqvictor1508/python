4fc1bd16463fdd0783b3104a16beeaf7f7faa917
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-04-25 20:53:29 -0300

    vito

---
+++ questao3.py
@@ -0,0 +1,7 @@
+a = float(input('Sua altura: '))
+
+peso1 = (72.7 * a) - 58
+peso2 = (62.1 * a) - 44.7
+
+print('Para homens:',peso1 )
+print('Para mulheres:',peso2)
---
+++ questao4.py
@@ -0,0 +1,36 @@
+a = 5
+b = 15
+c = 60
+d = 70
+j = 20
+k = 23
+z = 32
+
+
+t1 = a = b + z * b
+t2 = a = b + a
+t3 = a = b + j * k + 0.5 * a * a
+t4 = a = b = b * b + b * z * b
+t5 = c = b / 4
+t6 = c = b / a
+t7 = c = b / 7
+t8 = d = b / a
+t9 = d = b / a 
+t10 = c = b * j / k
+t11 = j = b / a
+
+
+
+
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+print(t5)
+print(t6)
+print(t7)
+print(t8)
+print(t9)
+print(t10)
+print(t11)
+
