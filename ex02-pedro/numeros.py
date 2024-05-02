d8ebef49a3b600731c4ae3b1aa99a0078f7a30ff
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-02 20:15:46 -0300

    Create numeros.py

---
+++ ex02-pedro/numeros.py
@@ -0,0 +1,12 @@
+a = int(input('1 para somar, 2 para subtrair'))
+
+n1 = float(input('Digite o primeiro numero'))
+n2 = float(input('Digite o segundo numero'))
+
+if a == 1:
+     n3 = n1 + n2
+     print(n3)
+else:
+     n4 = n1 - n2
+     print(n4)
+     
