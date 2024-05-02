7542c8a57587c6cfc42785c0101aa6f533632d9e
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-02 19:50:13 -0300

    123

--- ex02-pedro/idade.py
+++ ex02-pedro/idade.py
@@ -1,9 +1,9 @@
 idade = int(input("Digite sua idade:"))
 
-if idade > 18:
+if idade < 18:
     print("você é de menor")
 
-elif idade < 18:
+elif idade > 18:
     print("você é de maior")
 
 else:
---
+++ ex02-pedro/senha.py
@@ -0,0 +1,8 @@
+
+senha = 'soueu123'
+
+print('Digite sua senha:')
+
+if senha != 'soueu123':
+    print('senha inválida')
+
